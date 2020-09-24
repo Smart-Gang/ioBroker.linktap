330be72a27055bdc940ba369e544100754256b0f
Smart-Gang <gangrulez@gmail.com>
2020-09-24 21:43:59 +0200

    first commit with code

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# ioBroker.LinkTap
+![Logo](admin/Logo_small.png)
+# ioBroker.template
+
+[![NPM version](http://img.shields.io/npm/v/iobroker.template.svg)](https://www.npmjs.com/package/iobroker.template)
+[![Downloads](https://img.shields.io/npm/dm/iobroker.template.svg)](https://www.npmjs.com/package/iobroker.template)
+![Number of Installations (latest)](http://iobroker.live/badges/template-installed.svg)
+![Number of Installations (stable)](http://iobroker.live/badges/template-stable.svg)
+[![Dependency Status](https://img.shields.io/david/Author/iobroker.template.svg)](https://david-dm.org/Author/iobroker.template)
+[![Known Vulnerabilities](https://snyk.io/test/github/Author/ioBroker.template/badge.svg)](https://snyk.io/test/github/Author/ioBroker.template)
+
+[![NPM](https://nodei.co/npm/iobroker.template.png?downloads=true)](https://nodei.co/npm/iobroker.template/)
+
+**Tests:**: [![Travis-CI](http://img.shields.io/travis/Author/ioBroker.template/master.svg)](https://travis-ci.org/Author/ioBroker.template)
+
+## ioBroker.LinkTap
+
 Control your garden irrigation with the LinkTap Wireless Water Timer
+
+## Installation
+Was developed under Node.js 12. So it is recommended to use this version at least.
+
+## Settings
+Create Api key at https://www.link-tap.com/#!/api-for-developers with your LinkTap credentials.
+
+Please enter the username and API key in the configuration.
+All connected gateways and taplinkers will be retrieved after the adapter is started. The retrieval can be done again at the earliest after 5 minutes.
+The interval for retrieving the status information of the devices is every minute.
+
+## Changelog
+
+### 0.0.1
+* (Smart-Gang) initial release
+
+## License
+MIT License
+
+Copyright (c) 2020 Author <author@mail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ main.js
@@ -0,0 +1,410 @@
+  
+/**
+ *      LinkTap adapter
+ *
+ *      Controls LinkTap Wireless Water Timer via LinkTap Gateway
+ *
+ *      Copyright 2020 Smart-Gang <gangrulez@gmail.com>,
+ *      MIT License
+ *
+ */
+
+'use strict';
+
+/*
+ * Created with @iobroker/create-adapter v1.26.3
+ */
+
+// The adapter-core module gives you access to the core ioBroker functions
+// you need to create an adapter
+const utils = require('@iobroker/adapter-core');
+const LinkTapApiController = require("./linktap_api_controller");
+var myApiController = null;
+// Load your modules here, e.g.:
+// const fs = require("fs");
+
+class LinkTap extends utils.Adapter {
+
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+    constructor(options) {
+        super({
+            ...options,
+            name: 'linktap',
+        });
+        this.on('ready', this.onReady.bind(this));
+        this.on('stateChange', this.onStateChange.bind(this));
+        // this.on('objectChange', this.onObjectChange.bind(this));
+        // this.on('message', this.onMessage.bind(this));
+        this.on('unload', this.onUnload.bind(this));
+
+        this.connected = null;
+        this.dataPollInterval = 60000;
+        this.dataPollTimeout = null;
+
+    }
+
+    getId(gatewayId, taplinkerId, stateKey){
+        if(typeof(gatewayId) === 'undefined'  &&  typeof(taplinkerId) !== 'undefined' && typeof(stateKey) === 'undefined') {
+            return 'gateways';
+        }
+        if(typeof(taplinkerId) === 'undefined' && typeof(stateKey) === 'undefined'){
+            return 'gateways.'+gatewayId;
+        }     
+        if(typeof(gatewayId) !== 'undefined' && typeof(taplinkerId) !== 'undefined' && typeof(stateKey) === 'undefined'){
+            return 'gateways.'+gatewayId+'.'+taplinkerId
+        }            
+        if(typeof(taplinkerId) === 'undefined') {
+            return 'gateways.'+gatewayId+'.'+stateKey;
+        }
+        return 'gateways.'+gatewayId+'.'+taplinkerId+'.'+stateKey;        
+    }    
+
+    //Used for creating states
+    createState(name, value, desc, _write, _unit) {
+
+        if(typeof(desc) === 'undefined')
+            desc = name;
+        if(typeof(_write) === 'undefined')
+            _write = false;
+        if(typeof(_write) !== 'boolean')
+            _write = false;
+
+        if(Array.isArray(value))
+            value = value.toString();
+
+        if(typeof(_unit) === 'undefined') {
+            this.setObjectNotExists(name, {
+                type: 'state',
+                common: {
+                    name: name,
+                    desc: desc,
+                    type: typeof(value),
+                    read: true,
+                    write: _write
+                },
+                native: {id: name}
+            }, function(err, obj) {
+                if (!err && obj) {
+                    if(typeof(value) !== 'undefined') {
+                        this.setState(name, {
+                            val: value,
+                            ack: true
+                        });
+                    }
+                }
+            });
+        } else {
+            this.setObjectNotExists(name, {
+                type: 'state',
+                common: {
+                    name: name,
+                    desc: desc,
+                    type: typeof(value),
+                    read: true,
+                    write: _write,
+                    unit: _unit
+                },
+                native: {id: name}
+            }, function(err, obj) {
+                if (!err && obj) {
+                    if(typeof(value) !== 'undefined') {
+                        this.setState(name, {
+                            val: value,
+                            ack: true
+                        });
+                    }
+                }
+            });
+        }
+    } 
+
+    //Creates all channels
+    createChannels() {
+        const fctName = 'createChannels';
+        this.log.info(fctName + ' started');
+        this.setObjectNotExists('gateways', {
+            type: 'channel',
+            role: 'gateways',
+            common: {
+                name: this.getId(),
+            },
+            native: {}
+        }, function(err) {
+            if (err) {
+                this.log.error('Cannot write object: ' + err); 
+            }
+        });
+
+        if(myApiController != null ){
+            myApiController.gateways.forEach((g) => {
+                this.setObjectNotExists(g.gatewayId, {
+                    type: 'channel',
+                    role: 'gateway',
+                    common: {
+                        name: this.getId(g.gatewayId),
+                    },
+                    native: {}
+                }, function(err) {
+                    if (err) {
+                        this.log.error('Cannot write object: ' + err); 
+                    }
+                });  
+                g.devices.forEach(d => {
+                    this.setObjectNotExists(d.taplinkerId, {
+                        type: 'channel',
+                        role: 'device',
+                        common: {
+                            name:  this.getId(g.gatewayId, d.taplinkerId),
+                        },
+                        native: {}
+                    }, function(err) {
+                        if (err) {
+                            this.log.error('Cannot write object: ' + err); 
+                        }
+                    });                      
+                });              
+            });  
+        }    
+        this.log.debug(fctName + ' finished');    
+    } 
+
+    setStates(){
+        if(myApiController != null ){
+            myApiController.gateways.forEach((g) => {
+                this.setStateAsync(this.getId(g.gatewayId,null,'name'), { val: g.name, ack: true });
+                this.setStateAsync(this.getId(g.gatewayId,null,'status'), { val: g.status, ack: true });
+                this.setStateAsync(this.getId(g.gatewayId,null,'location'), { val: g.location, ack: true });
+                this.setStateAsync(this.getId(g.gatewayId,null,'version'), { val: g.version, ack: true });
+                g.devices.forEach(d => {
+                    this.setStateAsync(this.getId(g.gatewayId,d.taplinkerId,'taplinkerName'), { val: d.taplinkerName, ack: true });
+                    this.setStateAsync(this.getId(g.gatewayId,d.taplinkerId,'location'), { val: d.location, ack: true });
+                    this.setStateAsync(this.getId(g.gatewayId,d.taplinkerId,'status'), { val: d.status, ack: true });
+                    this.setStateAsync(this.getId(g.gatewayId,d.taplinkerId,'version'), { val: d.version, ack: true });
+                    this.setStateAsync(this.getId(g.gatewayId,d.taplinkerId,'signal'), { val: d.signal, ack: true });
+                    this.setStateAsync(this.getId(g.gatewayId,d.taplinkerId,'batteryStatus'), { val: d.batteryStatus, ack: true });
+                    this.setStateAsync(this.getId(g.gatewayId,d.taplinkerId,'workMode'), { val: d.workMode, ack: true });
+                    this.setStateAsync(this.getId(g.gatewayId,d.taplinkerId,'watering'), { val: d.watering, ack: true });
+                    this.setStateAsync(this.getId(g.gatewayId,d.taplinkerId,'vel'), { val: d.vel, ack: true });
+                    this.setStateAsync(this.getId(g.gatewayId,d.taplinkerId,'fall'), { val: d.fall, ack: true });
+                    this.setStateAsync(this.getId(g.gatewayId,d.taplinkerId,'valveBroken'), { val: d.valveBroken, ack: true });
+                    this.setStateAsync(this.getId(g.gatewayId,d.taplinkerId,'noWater'), { val: d.noWater, ack: true });
+                    this.setStateAsync(this.getId(g.gatewayId,d.taplinkerId,'total'), { val: d.total, ack: true });
+                    this.setStateAsync(this.getId(g.gatewayId,d.taplinkerId,'onDuration'), { val: d.onDuration, ack: true });
+                    this.setStateAsync(this.getId(g.gatewayId,d.taplinkerId,'ecoTotal'), { val: d.ecoTotal, ack: true });                    
+                    this.setStateAsync(this.getId(g.gatewayId,d.taplinkerId,'ecoOn'), { val: d.ecoOn, ack: true });
+                    this.setStateAsync(this.getId(g.gatewayId,d.taplinkerId,'ecoOff'), { val: d.ecoOff, ack: true });
+                });
+            });
+        }
+    }
+
+    createDPs() {
+
+        const fctName = 'createDPs';   
+        this.log.debug(fctName + ' started');
+    
+        if(myApiController != null ){
+            myApiController.gateways.forEach((g) => {
+                this.createState(this.getId(g.gatewayId,null,'id'), '');         
+                this.createState(this.getId(g.gatewayId,null,'name'), '');
+                this.createState(this.getId(g.gatewayId,null,'location'), '');
+                this.createState(this.getId(g.gatewayId,null,'status'), '');
+                this.createState(this.getId(g.gatewayId,null,'version'), '');
+                g.devices.forEach(d => {
+                    this.createState(this.getId(g.gatewayId,d.taplinkerId,'taplinkerName'), '');
+                    this.createState(this.getId(g.gatewayId,d.taplinkerId,'location'),'');
+                    this.createState(this.getId(g.gatewayId,d.taplinkerId,'taplinkerId'), '');
+                    this.createState(this.getId(g.gatewayId,d.taplinkerId,'status'), '');          
+                    this.createState(this.getId(g.gatewayId,d.taplinkerId,'version'), '');
+                    this.createState(this.getId(g.gatewayId,d.taplinkerId,'signal'), 0, undefined, false, '%');
+                    this.createState(this.getId(g.gatewayId,d.taplinkerId,'batteryStatus'), 0, undefined, false, '%');
+                    this.createState(this.getId(g.gatewayId,d.taplinkerId,'workMode'), '');
+                    this.createState(this.getId(g.gatewayId,d.taplinkerId,'watering'), '');
+                    this.createState(this.getId(g.gatewayId,d.taplinkerId,'vel'), 0, undefined, false, 'ml/min');                    
+                    this.createState(this.getId(g.gatewayId,d.taplinkerId,'fall'), false);                    
+                    this.createState(this.getId(g.gatewayId,d.taplinkerId,'valveBroken'), false);                    
+                    this.createState(this.getId(g.gatewayId,d.taplinkerId,'noWater'), false);                    
+                    this.createState(this.getId(g.gatewayId,d.taplinkerId,'total'), 0, undefined, false, 'min');                    
+                    this.createState(this.getId(g.gatewayId,d.taplinkerId,'onDuration'), 0,undefined, false, 'min');                    
+                    this.createState(this.getId(g.gatewayId,d.taplinkerId,'ecoTotal'),0, undefined, false, 'min');                    
+                    this.createState(this.getId(g.gatewayId,d.taplinkerId,'ecoOn'), 0, undefined, false, 'min');  
+                    this.createState(this.getId(g.gatewayId,d.taplinkerId,'ecoOff'), 0), undefined, false, 'min';                      
+                });
+            });
+        }            
+        this.log.debug(fctName + ' finished');    
+    }
+
+    /**
+     * Is called when databases are connected and adapter received configuration.
+     */
+    async onReady() {
+        await this.setConnected(false);
+
+        this.log.info('User : ' + this.config.txtUsername);
+
+        if (!this.config.txtUsername || !this.config.txtApiKey) {
+            this.log.warn('Please open Admin page for this adapter to set the username and the API key.');
+            return;
+        }                               
+        /*
+        For every state in the system there has to be also an object of type state
+        Here a simple template for a boolean variable named "testVariable"
+        Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
+        */
+        /*
+        await this.setObjectNotExistsAsync('testVariable', {
+            type: 'state',
+            common: {
+                name: 'testVariable',
+                type: 'boolean',
+                role: 'indicator',
+                read: true,
+                write: true,
+            },
+            native: {},
+        });
+        */
+        await this.createChannels();
+        await this.createDPs();
+        // In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
+        //this.subscribeStates('testVariable');
+        // You can also add a subscription for multiple states. The following line watches all states starting with "lights."
+        // this.subscribeStates('lights.*');
+        // Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
+        this.subscribeStates('*');
+
+        this.main();
+    }
+
+    /**
+     * Is called when adapter shuts down - callback has to be called under any circumstances!
+     * @param {() => void} callback
+     */
+    onUnload(callback) {
+        try {
+            // Here you must clear all timeouts or intervals that may still be active
+            // clearTimeout(timeout1);
+            // clearTimeout(timeout2);
+            // ...
+            // clearInterval(interval1);
+
+            callback();
+        } catch (e) {
+            callback();
+        }
+    }
+
+    // If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
+    // You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
+    // /**
+    //  * Is called if a subscribed object changes
+    //  * @param {string} id
+    //  * @param {ioBroker.Object | null | undefined} obj
+    //  */
+    // onObjectChange(id, obj) {
+    //     if (obj) {
+    //         // The object was changed
+    //         this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
+    //     } else {
+    //         // The object was deleted
+    //         this.log.info(`object ${id} deleted`);
+    //     }
+    // }
+
+    /**
+     * Is called if a subscribed state changes
+     * @param {string} id
+     * @param {ioBroker.State | null | undefined} state
+     */
+    onStateChange(id, state) {
+        if (state) {
+            // The state was changed
+            this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+        } else {
+            // The state was deleted
+            this.log.info(`state ${id} deleted`);
+        }
+    }
+
+    // If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
+    // /**
+    //  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
+    //  * Using this method requires "common.message" property to be set to true in io-package.json
+    //  * @param {ioBroker.Message} obj
+    //  */
+    // onMessage(obj) {
+    //     if (typeof obj === 'object' && obj.message) {
+    //         if (obj.command === 'send') {
+    //             // e.g. send email or pushover or whatever
+    //             this.log.info('send command');
+
+    //             // Send response in callback if required
+    //             if (obj.callback) this.sendTo(obj.from, obj.command, 'Message received', obj.callback);
+    //         }
+    //     }
+    // }
+
+
+    setConnected(isConnected) {
+        if (this.connected !== isConnected) {
+            this.connected = isConnected;
+            this.setState('info.connection', this.connected, true);
+        }
+    }
+    
+    main(){
+    
+        if (this.dataPollInterval !== 0) {
+            this.dataPollInterval = (this.config.txtPollInterval *60 * 1000) || this.dataPollInterval;
+        }    
+        
+        myApiController = new LinkTapApiController(this.config.txtUsername, this.config.txtApiKey);
+        this.setConnected(myApiController.connected);
+
+        this.setStates();
+        /*
+            setState examples
+            you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
+        */
+        // the variable testVariable is set to true as command (ack=false)
+        //this.setStateAsync('testVariable', true);
+
+        // same thing, but the value is flagged "ack"
+        // ack should be always set to true if the value is received from or acknowledged from the target system
+        //this.setStateAsync('testVariable', { val: true, ack: true });
+
+        // same thing, but the state is deleted after 30s (getState will return null afterwards)
+        //this.setStateAsync('testVariable', { val: true, ack: true, expire: 30 });
+
+    }
+    /*
+    startDataPolling(fromTimeout) {
+        if (this.dataPollTimeout) {
+            !fromTimeout && clearTimeout(this.dataPollTimeout);
+            this.dataPollTimeout = null;
+        }
+        if (this.dataPollInterval === 0) {
+            this.log.info('Data polling deactivated.');
+            return;
+        }
+        this.dssStruct.updateMeterData(() => {
+            this.dataPollTimeout = setTimeout(() => this.startDataPolling(true), this.dataPollInterval);
+        });
+    }    
+    */
+}
+
+// @ts-ignore parent is a valid property on module
+if (module.parent) {
+    // Export the constructor in compact mode
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+    module.exports = (options) => new LinkTap(options);
+} else {
+    // otherwise start the instance directly
+    new LinkTap();
+}
