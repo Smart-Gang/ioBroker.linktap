0a0fe87ab771cea1b6bf6fcb2d306026b338e7fc
Smart-Gang <gangrulez@gmail.com>
2024-09-30 17:41:18 +0200

    test import chaiaspromised

--- test/mocha.setup.js
+++ test/mocha.setup.js
@@ -6,9 +6,9 @@ process.on('unhandledRejection', (e) => {
 // enable the should interface with sinon
 // and load chai-as-promised and sinon-chai by default
 const sinonChai = require('sinon-chai');
-const chaiAsPromised = (...args) => import('chai-as-promised').then(({default: chaiAsPromised}) => chaiAsPromised(...args));
+import * as chaiAsPromised from "chai-as-promised";
 const { should, use } = require('chai');
 
 should();
 use(sinonChai);
-use(chaiAsPromised);
+use((chaiAsPromised as any).default);
