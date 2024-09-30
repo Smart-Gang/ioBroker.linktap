bed0145558f89e719a5cf33d4f03b663afd1e5ea
Smart-Gang <gangrulez@gmail.com>
2024-09-30 17:44:23 +0200

    removed chai as promised

--- test/mocha.setup.js
+++ test/mocha.setup.js
@@ -6,9 +6,7 @@ process.on('unhandledRejection', (e) => {
 // enable the should interface with sinon
 // and load chai-as-promised and sinon-chai by default
 const sinonChai = require('sinon-chai');
-import * as chaiAsPromised from "chai-as-promised";
 const { should, use } = require('chai');
 
 should();
 use(sinonChai);
-use((chaiAsPromised as any).default);
